9027fd1497001cdd10277f3c9f5f362bbe874c0a
Matt Stratton <matt.stratton@gmail.com>
2016-10-27 00:26:09 -0500

    Update metadata.rb with proper details

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,9 @@
 name 'aar_cookbook'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures aar_cookbook'
-long_description 'Installs/Configures aar_cookbook'
+maintainer 'Matt Stratton'
+maintainer_email 'matt.stratton@gmail.com'
+license 'Apache 2.0'
+description 'Installs and configures the Awesome Appliance Repair application'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/aar_cookbook/issues' if respond_to?(:issues_url)
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/aar_cookbook' if respond_to?(:source_url)
+issues_url 'https://github.com/mattstratton/aar_cookbook/issues'
+source_url 'https://github.com/mattstratton/aar_cookbook'

1de96dd7df77983a8bd40a4cb3b2ca16c77d41fe
Matt Stratton <matt.stratton@gmail.com>
2016-12-12 19:55:59 -0600

    Add centos testing to kitchen
    Signed-off-by: Matt Stratton <matt.stratton@gmail.com>

--- test/recipes/web.rb
+++ test/recipes/web.rb
@@ -5,7 +5,14 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-describe package('apache2') do
+case os['family']
+when 'debian'
+  apache_package = 'apache2'
+when 'redhat'
+  apache_package = 'httpd'
+end
+
+describe package(apache_package) do
   it { should be_installed }
 end
 
@@ -13,13 +20,13 @@ describe port(80) do
   it { should be_listening }
 end
 
-describe file('/etc/apache2/sites-enabled/AAR-apache.conf') do
+describe file("/etc/#{apache_package}/sites-enabled/AAR-apache.conf") do
   it { should be_file }
   its('content') { should match(/ServerName/) }
   it { should be_owned_by 'root' }
 end
 
-describe systemd_service('apache2') do
+describe systemd_service(apache_package) do
   it { should be_installed }
   it { should be_enabled }
   it { should be_running }
